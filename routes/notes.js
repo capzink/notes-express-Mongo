393a342c6f9f0cce017bd6fba91de2a2b20b2dc0
Carlos <capz2003@gmail.com>
2021-11-03 15:19:30 -0500

    agrega funcionalidad de form

--- routes/notes.js
+++ routes/notes.js
@@ -7,6 +7,16 @@ router.get('/notes/add',(req,res)=>{
     res.render('notes/newnote')
 })
 
+router.get('/notes',(req,res)=>{
+    res.send('sgnin')
+})
+
+router.post('/notes/newnote',(req,res)=>{
+    console.log(req.body)
+    res.send('ok')
+
+})
+
 
 
 
