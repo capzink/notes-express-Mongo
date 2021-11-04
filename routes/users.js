e1b37486b158dce356da98b406be5596b3bc5c36
Carlos <capz2003@gmail.com>
2021-11-04 17:06:04 -0500

    added signup with encryption

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,8 @@
 const router =require('express').Router();
 
+//para usar base de datos este es el modulo de usuario instancia
+const user =require('../models/user')
+
 //ruta de autenticacion
 //sign in process
 
@@ -13,11 +16,15 @@ router.get('/users/signin',(req,res)=>{
 
 //ruta para recibir datos
 
-/*router.post('/users/signup', (req,res)=>{
+router.post('/users/signup', async (req,res)=>{
    const {name, password, email, confirm_password}=req.body
    const errors=[];
+    if(name.length===0 || email.length===0){
+        errors.push({text:' please type in Name'})
+    }
+
    if(password!==confirm_password){
-       errors.push({text: 'password do not match'})
+       errors.push({text: 'password does not match'})
    }
    if(password.length<4){
        errors.push({text:"Password must be at least 4 characters"})
@@ -26,9 +33,22 @@ router.get('/users/signin',(req,res)=>{
        res.render('users/signup',{errors,name,password,confirm_password})
 
    }else{
-    res.send('ok')
+    //verificacion que correo no exsita ya
+    const emailUser= await user.findOne({email:'email'})
+    if(emailUser){
+        req.flash('error_msg', 'email already exists')
+        res.redirect('/users/signup')
+    }
+    //creacion de nuevo usuario
+    const newUser= new user({name,email,password})
+    //guardar contrasena cifrada
+    newUser.password= await newUser.encryptPassword(password)
+    //guardar usuario
+    await newUser.save()
+    req.flash('succes_msg', 'You Are Registered')
+    res.redirect('/users/signin')
    }
-})*/
+})
 
 
 
