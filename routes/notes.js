d8bf06f27410b96158c214424022beca6922f408
Carlos <capz2003@gmail.com>
2021-11-03 19:36:16 -0500

    added errors control empty form

--- routes/notes.js
+++ routes/notes.js
@@ -8,16 +8,25 @@ router.get('/notes/add',(req,res)=>{
 })
 
 router.get('/notes',(req,res)=>{
-    res.send('sgnin')
+    res.send('signin')
 })
 
 router.post('/notes/newnote',(req,res)=>{
     console.log(req.body)
-    res.send('ok')
-
+    const {title, description}=req.body;
+    const errors=[];
+    if(!title){
+       errors.push({text: 'please write a title'});
+    }
+    if(!description){
+       errors.push({text:'please write a description'});
+    }
+    if(errors.length>0){
+        res.render('notes/newnote', {errors,title,description});
+    }
+    else{
+    res.send('ok');
+    }
 })
 
-
-
-
-module.exports =router
+module.exports =router;
