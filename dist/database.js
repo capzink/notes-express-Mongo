ef2081b8dfc18ba9897a187cdf80b9827b146712
Carlos <capz2003@gmail.com>
2021-12-20 10:59:05 -0500

    l

--- dist/database.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose'); //conectar mongoose funcionamineto biblioteca
-
-
-const connectDB = url => {
-  return mongoose.connect(url, {});
-};
-
-module.exports = connectDB;
